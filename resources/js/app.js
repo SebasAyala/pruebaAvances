1d8d77b4015f9c8683619983d4695b7e02045a79
sebas7603 <sebastianutpae@gmail.com>
2020-08-20 09:39:32 -0500

    Intalación de dependencias de NPM y configuración de vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,33 @@
 require('./bootstrap');
+
+window.Vue = require('vue')
+
+//Imports
+import Router from 'vue-router'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(Router)
+Vue.use(BootstrapVue)
+
+let router = new Router({
+    routes: [
+        {
+            path: '/',
+            component: {
+                template: '<div><h1>Login</h1></div>',
+            }
+        },
+        {
+            path: '/productos',
+            component: {
+                template: '<h1>Prod</h1>'
+            }
+        }
+    ]
+});
+
+const app = new Vue({
+    el: '#app',
+    router,
+    BootstrapVue
+});
